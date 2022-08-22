144b864c247a412ec90de71b1175f12ef2c54a6f
Simone <ssimone12@gmail.com>
2022-08-22 13:11:57 +0200

    opgave 1 done

--- oevelse_1.js
+++ oevelse_1.js
@@ -1,12 +1,18 @@
-document.addEventListener("DOMContentLoaded", funktionDerKaldesVedKlik);
-const minKnap = document.querySelector("#knapElement");
-minKnap.addEventListener("click", funktionDerKaldesVedKlik);
+const Cola = document.querySelector("#Cola");
+const sodavand = document.querySelector("#sodavand");
+const tuborg = document.querySelector("#tuborg");
+const snaps = document.querySelector("#snaps");
+
+Cola.addEventListener("click", funktionDerKaldesVedKlik);
+sodavand.addEventListener("click", funktionDerKaldesVedKlik);
+tuborg.addEventListener("click", funktionDerKaldesVedKlik);
+snaps.addEventListener("click", funktionDerKaldesVedKlik);
 
 function funktionDerKaldesVedKlik() {
   console.log("du har klikket på knappen");
-  if ((this.alt = "snaps")) {
+  if (this.alt == "snaps" || this.alt == "tuborg") {
     console.log("indeholder alkohol");
-  } else if ((this.alt = "tuborg")) console.log("indeholder alkohol");
-  else this.alt = "Cola";
-  console.log("alkoholfri");
+  } else {
+    console.log("alkoholfri");
+  }
 }
