843b496373546cdd5dbab5c524b1b949e713daf4
ctrl-Alx-del <electromanati@gmail.com>
2022-08-22 13:18:47 +0200

    opgave 2 done

--- oevelse_2/oevelse_2.js
+++ oevelse_2/oevelse_2.js
@@ -2,13 +2,11 @@ let time = new Date().getHours();
 
 
 
-
-
-if (time>= 5 || time <= 10) {
+if (time>= 5 && time <= 10) {
   console.log("Godmorgen");
-} else if (time>= 10  || time <=18 ) {
+} else if (time>= 10  && time <=18 ) {
   console.log("Goddag");
-} else if (time>=  18|| time<= 24){
+} else if (time>=  18 && time<= 24){
   console.log("Godaften");
 } else {
   console.log("Godnat");
