a5afcc817ff6fee49550fff5123b52cd54bf785b
Magnus Bolton <magnus@bolton.dk>
2022-08-23 12:36:40 +0200

    ny

--- oevelse_3/oevelse_3.js
+++ oevelse_3/oevelse_3.js
@@ -1,6 +1,19 @@
-
 /* Definere input felt */
-<input type="number" id="tal">
+knap.addEventListener("click", brugknap);
+var gæt = document.querySelector("#tal").value;
+var tal = math.floor(Math.random() * 10);
+var knap = document.querySelector("button");
 
-/* værdien for inputfelt */
-document.querySelectorSelector("#tal").value
+function rigtigt() {
+  if (tal == gæt) {
+    alert("Du har vundet!");
+  }
+}
+function forkert() {
+  if (tal !== gæt) {
+    alert("Du har tabt!");
+  }
+}
+function brugknap() {
+  console.log("Klik på knap");
+}
